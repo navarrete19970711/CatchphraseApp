2a7d4829bed2cda3ca3313894765bc881397f79f
navarrete19970711 <mark.joseph.navarrete@gmail.com>
2024-09-06 11:49:30 +0900

    バックエンド実装
    遷移機能、受け取った値を遷移先に渡す機能、ランダムでキャッチコピーを生成して表示させる機能

--- app/src/main/java/com/example/catchphraseapp/MainActivity.kt
+++ app/src/main/java/com/example/catchphraseapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.catchphraseapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,5 +21,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 1) viewを取得
+        val et:EditText = findViewById(R.id.ed)
+        val btnStart:Button = findViewById(R.id.btnStart)
+
+        // 2) 画面を渡す
+        btnStart.setOnClickListener {
+            if (et.text.toString() == ""){
+                Toast.makeText(this,"なんか書いて",Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this,ResultActivity::class.java)
+                // 3) 値を渡す
+                intent.putExtra("MY_ITEM",et.text.toString())
+                startActivity(intent)
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/example/catchphraseapp/ResultActivity.kt
+++ app/src/main/java/com/example/catchphraseapp/ResultActivity.kt
@@ -1,10 +1,13 @@
 package com.example.catchphraseapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Random
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,45 @@ class ResultActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tvResult:TextView = findViewById(R.id.tvResult)
+        val btnReplay:Button = findViewById(R.id.btnReplay)
+
+        // 4) 値を受け取る→テキストに表示
+        val myItem = intent.getStringExtra("MY_ITEM")
+        //tvResult.text = myItem
+
+        // 5) 配列を用意
+        val msgData = arrayOf(
+            " 女子高生が選ぶ『 $myItem 』ランキング",
+            "100円ショップにあるものだけで『 $myItem 』を作る",
+            " 世界の富豪が実践する３つの『 $myItem』",
+            " 逆に今『 $myItem 』がナウい！",
+            " 世界は『 $myItem 』で出来ている",
+            " 彼のハートを打ち抜く神『 $myItem 』",
+            " 衝撃！『 $myItem 』だけで１ヵ月生活",
+            " あなたの知らない『 $myItem 』の世界",
+            " 今からはじめる『 $myItem 』",
+            " 乳酸菌が１００億個入った『 $myItem 』",
+            " 100年に1度の『 $myItem 』",
+            " 愛と『 $myItem 』の物語",
+            " 世界よ！これが真の『 $myItem 』だ！！",
+            " あのカリスマ『 $myItem 』、待望の映画化！",
+            " 今年最高の『 $myItem 』をお届け",
+            " あの素晴らしい『 $myItem 』をもう一度",
+        )
+
+        // 7) 乱数を作る
+        //val num = Random().nextInt(16)
+        val num = Random().nextInt(msgData.count())
+
+        // 6) 配列の0番目を表示
+        tvResult.text = msgData[num]
+
+        // 9) リプレイボタン
+        btnReplay.setOnClickListener {
+            finish()
+        }
+
     }
 }
